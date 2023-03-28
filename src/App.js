42a8aca2f9d18af8e51412790b2e8ac8e95c3ba4
Jayesh-2001 <jayeshborole2002@gmail.com>
2023-03-28 12:45:58 +0530

    Version 1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
 import './App.css';
 import React, { Component } from 'react'
 
-export class App extends Component {
-   render() {
-      return (
-         <div className='App'>
-            <header>
-               <form id='to-do-form'>
-                  <input type="text" placeholder='Enter text' />
-                  <button type="submit">Add</button>
-               </form>
-            </header>
-         </div>
-      )
-   }
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      items: [],
+      currentItem: {
+        text: '',
+        key: ''
+      }
+    }
+    this.handleInput = this.handleInput.bind(this);
+    this.addItem = this.addItem.bind(this);
+
+  }
+  handleInput(e) {
+    this.setState({
+      currentItem: {
+        text: e.target.value,
+        key: Date.now()
+      }
+    })
+  }
+  addItem(e) {
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    console.log(newItem)
+  }
+  render() {
+    return (
+      <div className='App'>
+        <header>
+          <form id='to-do-form' onSubmit={this.addItem} >
+            <input type="text" placeholder="Enter Text"
+              value={this.state.currentItem.text}
+              onChange={this.handleInput}
+            />
+            <button type='submit'>Add</button>
+          </form>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App
